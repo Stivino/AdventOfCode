a36e5f122000e603b1c13338fc053dd0fcaa2ae3
Steven Weber <steven.weber@orbis.de>
2021-12-02 17:11:51 +0100

    Solved Day2

--- AdventOfCode/Day_01.cs
+++ AdventOfCode/Day_01.cs
@@ -1,15 +1,39 @@
-﻿namespace AdventOfCode;
+﻿namespace AdventOfCode {
 
 public class Day_01 : BaseDay
-{
-    private readonly string _input;
-
-    public Day_01()
     {
-        _input = File.ReadAllText(InputFilePath);
-    }
+        private readonly int[] _input;
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+        public Day_01()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(Int32.Parse).ToArray();
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        public override ValueTask<string> Solve_1() {
+            List<int> depths = new List<int>();
+            int lastDepth = -1;
+            int increasedCtr = 0;
+            foreach (int depth in _input) {
+                if (lastDepth > 0 && depth > lastDepth) {
+                    increasedCtr += 1;
+                }
+                lastDepth = depth;
+            }
+            return new (increasedCtr.ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            int lastDepth = -1;
+            int increasedCtr = 0;
+            int depth = 0;
+            for (int i = 0; i < _input.Length - 2; i++) {
+                depth = _input[i] + _input[i + 1] + _input[i + 2];
+                if (lastDepth > 0 && depth > lastDepth)
+                    increasedCtr++;
+                lastDepth = depth;
+            }
+            return new(increasedCtr.ToString());
+        }
+    } 
 }
---
+++ AdventOfCode/Day_02.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode
+{
+
+    public class Day_02 : BaseDay
+    {
+
+        private readonly string[][] _input;
+
+
+        public Day_02()
+        {
+            _input = File.ReadAllLines(InputFilePath).Select(line => line.Split(' ')).ToArray();
+        }
+
+        public override ValueTask<string> Solve_1()
+        {
+            Int64 horizontal = 0;
+            Int64 vertical = 0;
+
+            for (int i = 0; i < _input.Length; i++) {
+                int val = int.Parse(_input[i][1]);
+                switch (_input[i][0]) {
+                    case "forward":
+                        horizontal += val;
+                        break;
+                    case "down":
+                        vertical += val;
+                        break;
+                    case "up":
+                        vertical -= val;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return new((horizontal * vertical).ToString());
+        }
+
+        public override ValueTask<string> Solve_2()
+        {
+            Int64 horizontal = 0;
+            Int64 vertical = 0;
+            Int64 aim = 0;
+
+            for (int i = 0; i < _input.Length; i++) {
+                int val = int.Parse(_input[i][1]);
+                switch (_input[i][0]) {
+                    case "forward":
+                        horizontal += val;
+                        if (aim <= 0)
+                            break;
+                        vertical += aim * val;
+                        break;
+                    case "down":
+                        aim += val;
+                        break;
+                    case "up":
+                        aim -= val;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return new((horizontal * vertical).ToString());
+        }
+    }
+}
